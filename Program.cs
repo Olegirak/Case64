98c39e2aaa62741046716823b9b47f461ade6876
Oleg Iraklionov <irakliov@mail.ru>
2023-02-03 20:35:20 +0300

    Case 64 is ready

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу,
+ которая выведет все натуральные числа в промежутке от N до 1. 
+ Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+int GetDataFromUser(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    System.Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()!);
+    Console.ResetColor();
+    return result;
+}
+string RecNatNum(int n, string res)
+{
+    if (n > 0)
+    {
+        res = n + ", " + RecNatNum(n - 1, res);
+    }
+    return res;
+}
+int x = GetDataFromUser("Введите число N");
+string res = string.Empty;
+res = RecNatNum(x, res);
+res = '"' + res[..^2] + '"';
+Console.WriteLine(res);
